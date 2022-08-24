9989b8c06aee81f56e663831d2be8df65cf8c943
coderwqb <coderwqb@163.com>
2022-08-24 13:54:01 +0800

    third commit

--- src/main/java/com/atguigu/spring/GitTest.java
+++ src/main/java/com/atguigu/spring/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.spring;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git!!");
     }
 }
